ff62511ec3111ee103b8791a18355f13d4814709
florianrhein <florian.rhein@pickware.de>
2022-10-14 12:12:24 +0200

    Initial Commit

---
+++ WannVerbindung Watch App/WannVerbindungApp.swift
@@ -0,0 +1,17 @@
+//
+//  WannVerbindungApp.swift
+//  WannVerbindung Watch App
+//
+//  Created by Florian Rhein on 14.10.22.
+//
+
+import SwiftUI
+
+@main
+struct WannVerbindung_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WannVerbindung/WannVerbindungApp.swift
@@ -0,0 +1,17 @@
+//
+//  WannVerbindungApp.swift
+//  WannVerbindung
+//
+//  Created by Florian Rhein on 14.10.22.
+//
+
+import SwiftUI
+
+@main
+struct WannVerbindungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
